56218c0248febc1d8c3214713216741c65fdfe5a
Anne <annevalder@yahoo.de>
2022-02-09 16:06:47 +0100

    add at file

---
+++ Code/proc_trendecon_at.R
@@ -0,0 +1,26 @@
+#' Calculate Trendecon Main Indices for Germany
+#'
+#' @export
+#'
+proc_trendecon_at <- function() {
+
+  kw_trendecon <- c(
+    "Wirtschaftskrise",
+    "Kurzarbeit",
+    "arbeitslos",
+    "Insolvenz"
+  )
+
+  proc_index(kw_trendecon, "AT", 'trendecon')
+
+  indices_in_production <- c(
+    "trendecon"
+  )
+
+  # copy to data/at
+  lapply(indices_in_production, function(e) fs::file_copy(path_keyword(e, "at", "sa"), path_data("at"), overwrite = TRUE))
+
+  # FIXME: also store d, w, m, f in data/de
+}
+
+
