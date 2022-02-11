c2df52c0b1f3e05805be3a10aaf9b98c78fe6ae2
anneval <72460239+anneval@users.noreply.github.com>
2022-02-11 09:01:14 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -6,6 +6,8 @@ get_stage("deploy") %>%
   add_step(step_install_github("trendecon/trendecon")) %>%
   add_step(step_install_cran("prophet")) %>%
   add_step(step_install_cran("tibble")) %>%
+  add_step(step_install_cran("httr")) %>%
+  add_code_step(withr::with_package("httr", set_config(config(http_version = 0)))) %>%
   add_code_step(library(prophet)) %>%
   add_code_step(source("https://raw.githubusercontent.com/anneval/MA/main/Code/Indicators.R")) %>%
   add_step(step_do_push_deploy())
