47591ce55eb1decf79d3cf5ce0a484817d63ff0a
anneval <72460239+anneval@users.noreply.github.com>
2022-02-11 09:17:52 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -9,5 +9,5 @@ get_stage("deploy") %>%
   add_step(step_install_cran("httr")) %>%
   add_code_step(withr::with_package("httr", set_config(config(http_version = 0)))) %>%
   add_code_step(library(prophet)) %>%
-  add_code_step(source("https://raw.githubusercontent.com/anneval/MA/main/Code/Indicators.R")) %>%
+  add_code_step(source("https://raw.githubusercontent.com/anneval/MA/main/Indicators.R")) %>%
   add_step(step_do_push_deploy())
